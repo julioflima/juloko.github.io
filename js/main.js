79b5031a3c09e7b3a13c17038ecf4b1ca46a1f00
juloko <julio_flima@hotmail.com>
2018-05-08 00:25:36 -0300

    done

--- js/main.js
+++ js/main.js
@@ -1,36 +1,45 @@
-var nameInput = document.querySelector("#name");
-var emailInput = document.querySelector("#email");
-var messageInput = document.querySelector("#message");
-var submitButton = document.querySelector("#submit");
-var emailTo = "julio_flima@hotmail.com";
+            var emailTo = "julio_flima@hotmail.com";
 var subjectTo = "CV Git";
 var tokenTo = "a56cb4a3-c166-4d96-b5ec-40345a5b311b"
+var field = "You have to fill the field ";
+var fieldTwo = 'You have to fill the field email correctly, including and "@" and a"domain.example"';;
 
 
 function onSubmit() {
-    if (nameInput.value === "") {
-        alert("You have to fill the field name.");
+    if ($("#name").val() === "") {
+        warningAlert(field + "name")
+        setTimeout(function () {
+            $(".alert").alert('close');
+        }, 2000);
         return false;
 
     }
-    if (emailInput.value === "") {
-        alert("You have to fill the field email.");
+    if ($("#email").val() === "") {
+        warningAlert(field + "email")
+        setTimeout(function () {
+            $(".alert").alert('close');
+        }, 2000);
         return false;
 
     }
-    if (messageInput.value === "") {
-        alert("You have to fill the field message.");
+    if ($("#message").val() === "") {
+        warningAlert(field + "message")
+        setTimeout(function () {
+            $(".alert").alert('close');
+        }, 2000);
         return false;
     }
 
-    if (validationEmail(emailInput.value)) {
+    if (validationEmail($("#email").val())) {
         console.log("email OK");
     } else {
-        alert('You have to fill the field email correctly, including and "@" and a"domain.exemple".');
+        warningAlert(fieldTwo)
+        setTimeout(function () {
+            $(".alert").alert('close');
+        }, 2000);
         return false;
     }
 }
-
 
 function validationEmail(str) {
     if (str.search("@") > 0) {
@@ -43,4 +52,14 @@ function validationEmail(str) {
             return false;
         }
     }
+}
+
+function warningAlert(str) {
+    $("#alerting").html('\
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">\
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">\
+        <span aria-hidden="true">&times;</span>\
+    </button>\
+    <strong>Warning!</strong> ' + str + '.\
+</div>');
 }
